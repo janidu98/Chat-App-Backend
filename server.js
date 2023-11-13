5b6bab7b2eee9921aaafcc045d909d28a0d35436
janidu98 <janinduchathushan@gmail.com>
2023-11-13 13:41:43 +0530

    commit 1

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const { chats } = require('./data/data');
+
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find(chat => chat._id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`SERVER IS RUNNING ON PORT ${PORT}...`);
+})
