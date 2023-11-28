5052bd13f47cb16655ea9838e4bf68aab0e3604d
janidu98 <janinduchathushan@gmail.com>
2023-11-29 00:32:57 +0530

    send message API (one to one and  group chat

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -1,4 +1,6 @@
+const Chat = require('../models/chatModel');
 const Message = require('../models/messageModel');
+const User = require('../models/userModel');
 
 const sendMessage = async(req, res) => {
 
@@ -17,10 +19,37 @@ const sendMessage = async(req, res) => {
 
     try {
         var message = await Message.create(newMessage);
+
+        message = await message.populate('sender', 'name pic');
+        message = await message.populate('chat');
+        message = await User.populate(message, {
+            path: 'chat.users',
+            select: 'name pic email',
+        });
+
+        await Chat.findByIdAndUpdate(req.body.chatId, {
+            latestMessage: message,
+        });
+
+        res.json(message);
+
     } catch (error) {
-        
+        return res.status(400).send(error.message);
     }
 
 }
 
-module.exports = { sendMessage };
+const allMessages = async(req, res) => {
+    try {
+        const messages = await Message.find({ chat: req.params.chatId })
+        .populate('sender', 'name pic email')
+        .populate('chat');
+
+        res.json(messages);
+
+    } catch (error) {
+        return res.status(400).send(error.message);
+    }
+}
+
+module.exports = { sendMessage, allMessages };
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const protect = require('../middleware/authMiddleware');
-const { sendMessage } = require('../controllers/messageControllers');
+const { sendMessage, allMessages } = require('../controllers/messageControllers');
 
 const router = express.Router();
 
 router.post('/', protect, sendMessage);
+router.get('/:chatId', protect, allMessages);
 
 module.exports = router;
